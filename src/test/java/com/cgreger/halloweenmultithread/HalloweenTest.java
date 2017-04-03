8f1a213870ae404723dc6fda52f38b8c1199b27a
cgreger <greger@madisoncollege.edu>
2017-04-03 18:28:37 -0500

    solved & commented

--- src/test/java/com/cgreger/halloweenmultithread/HalloweenTest.java
+++ src/test/java/com/cgreger/halloweenmultithread/HalloweenTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by katana on 4/1/17.
- */
 public class HalloweenTest {
     @Test
     public void simulateHalloween() throws Exception {
